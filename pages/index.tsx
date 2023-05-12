a63a865d33f358abc95f56de4badf0d4f1584e6b
ryan-kim-dev <ryankim.h.dev@gmail.com>
2023-05-12 22:45:00 +0900

    Design: Practice basic tailwindCSS usage

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,36 @@
 import type { NextPage } from 'next';
-import Head from 'next/head';
-import Image from 'next/image';
-import styles from '../styles/Home.module.css';
+// import Head from 'next/head';
+// import Image from 'next/image';
+// import styles from '../styles/Home.module.css';
 
 const Home: NextPage = () => {
-  return <div>Start</div>;
+  return <div className='bg-slate-200 py-20 px-10 grid gap-10'>
+    <div className='bg-white p-10 rounded-xl shadow-xl'>
+    <span className="font-semibold text-3xl">Select Item</span>
+        <div className="flex justify-between my-2">
+          <span className="text-gray-500">Grey Chair</span>
+          <span className="font-semibold">$19</span>
+        </div>
+        <div className="flex justify-between">
+          <span className="text-gray-500">Grey Chair</span>
+          <span className="font-semibold">$19</span>
+        </div>
+        <div className="flex justify-between mt-2 pt-2 border-t-2 border-dashed">
+          <span>Total</span>
+          <span className="font-semibold">$10</span>
+        </div>
+        <div
+          className="mt-5 bg-blue-500 text-white p-3
+          text-center rounded-xl w-2/4 mx-auto
+         "
+        >
+          Checkout
+        </div>
+    </div>
+    <div className='bg-white p-10 rounded-xl shadow-xl'></div>
+    <div className='bg-white p-10 rounded-xl shadow-xl'></div>
+    <div className='bg-white p-10 rounded-xl shadow-xl'></div>
+  </div>;
 };
 
 export default Home;
