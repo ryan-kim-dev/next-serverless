128d8d4b8add0f5cec29330c471f5328f8bb4fc7
ryan-kim-dev <ryankim.h.dev@gmail.com>
2023-05-15 23:50:58 +0900

    temp

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,31 +5,16 @@ import type { NextPage } from 'next';
 
 const Home: NextPage = () => {
   return <div className='bg-slate-200 py-20 px-10 grid gap-10'>
-    <div className='bg-white p-10 rounded-xl shadow-xl'>
-    <span className="font-semibold text-3xl">Select Item</span>
-        <div className="flex justify-between my-2">
-          <span className="text-gray-500">Grey Chair</span>
-          <span className="font-semibold">$19</span>
-        </div>
-        <div className="flex justify-between">
-          <span className="text-gray-500">Grey Chair</span>
-          <span className="font-semibold">$19</span>
-        </div>
-        <div className="flex justify-between mt-2 pt-2 border-t-2 border-dashed">
-          <span>Total</span>
-          <span className="font-semibold">$10</span>
-        </div>
-        <div
-          className="mt-5 bg-blue-500 text-white p-3
-          text-center rounded-xl w-2/4 mx-auto
-         "
-        >
-          Checkout
-        </div>
+    <div className='bg-white p-6
+     rounded-xl shadow-xl'></div>
+    <div className='bg-white p-6
+     rounded-xl shadow-xl'></div>
+    <div className='bg-white p-6
+     rounded-xl shadow-xl'>
+      <div>
+        <span></span>
+      </div>
     </div>
-    <div className='bg-white p-10 rounded-xl shadow-xl'></div>
-    <div className='bg-white p-10 rounded-xl shadow-xl'></div>
-    <div className='bg-white p-10 rounded-xl shadow-xl'></div>
   </div>;
 };
 
