438d63937ec20b201bd73f0e528a0a352271b32e
ryan-kim-dev <ryankim.h.dev@gmail.com>
2023-06-01 11:41:24 +0900

    Temp: test api call for iwork automation

--- app/page.tsx
+++ app/page.tsx
@@ -5,15 +5,18 @@ import axios from 'axios';
 export default function Home() {
 	const [data, setData] = useState(null);
 
-	useEffect(() => {
+	const handleFetchData = () => {
 		axios.get('http://localhost:8080/reservations').then((res) => {
 			setData(res.data);
 		});
-	}, []);
+	};
+
+	useEffect(() => {}, []);
 
 	return (
 		<div>
 			<pre>{data}</pre>
+			<button onClick={handleFetchData}>Fetch Data</button>
 		</div>
 	);
 }
